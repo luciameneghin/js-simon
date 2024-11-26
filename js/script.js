c82c14437088846d669823d869ffeeae6b083320
luciameneghin <lucia.meneghin00@gmail.com>
2024-11-26 15:30:34 +0100

    feat: random number

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const counter = document.getElementById('counter');
-let seconds = 10;
+let randomGenerateNumber = document.getElementById('random-Num')
 
+
+
+let seconds = 10;
 counter.innerHTML = seconds--;
 
 const stopCounter = setInterval(function () {
@@ -10,3 +13,14 @@ const stopCounter = setInterval(function () {
   }
   seconds--
 }, 1000)
+
+
+
+randomGenerateNumber = [];
+for (let i = 1; i <= 5; i++) {
+  randomNumber = Math.floor(Math.random() * 50 + 1);
+  randomGenerateNumber.push(randomNumber)
+  console.log(randomGenerateNumber);
+}
+
+document.getElementById('random-Num').innerHTML = randomGenerateNumber.join(' - ');
